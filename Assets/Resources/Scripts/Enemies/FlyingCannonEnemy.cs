07dc7169bc8a4a51a6c57a06e0343ec3fd87228c
IanTheWatson <ian@devolutious.co.nz>
2016-03-26 11:55:02 +1300

    Crappy enemy movement stuff

--- Assets/Resources/Scripts/Enemies/FlyingCannonEnemy.cs
+++ Assets/Resources/Scripts/Enemies/FlyingCannonEnemy.cs
@@ -8,22 +8,35 @@ public class FlyingCannonEnemy : MonoBehaviour {
     private Transform _cannon;
 
     [Header("Movement")]
-    [SerializeField]
-    float MaximumSpeed = 50f;
+    [SerializeField] float MaximumSpeed = 60f;
+    [SerializeField] float BodyTurnRate = 5f;
 
-    float MaxCannonAngle = 30f;
-    float CannonAimRate = 0.5f;
+    [Header("Aiming")]
+    [SerializeField] float MaxCannonAngle = 30f;
+    [SerializeField] float CannonTurnRate = 0.25f;
 
     public Vector3 DesiredMovement
     {
         get;
         set;
-    }    
+    }   
+    
+    public Vector3 DesiredBodyRotation
+    {
+        get;
+        set;
+    }
+
+    public Vector2 DesiredCannonRotation
+    {
+        get;
+        set;
+    }
 
 	// Use this for initialization
 	void Start () {
         _rigidBody = GetComponentInChildren<Rigidbody>();
-        _cannon = GetComponentsInChildren<Transform>().FirstOrDefault(t => t.name == "CannonParent");
+        _cannon = GetComponentsInChildren<Transform>().FirstOrDefault(t => t.name == "Cannon");
 	}
 	
 	// Update is called once per frame
@@ -31,39 +44,125 @@ public class FlyingCannonEnemy : MonoBehaviour {
 	    
 	}
 
+    //If not rotating on X, rotating on Y
+    void RotateCannon(float rotationChange, bool rotateOnX)
+    {
+        if (rotationChange == 0)
+        {
+            return;
+        }
+
+        if (Mathf.Abs(rotationChange) > CannonTurnRate)
+        {
+            rotationChange = rotationChange > 0 ? CannonTurnRate : -CannonTurnRate;
+        }
+
+        if (rotateOnX)
+        {
+            rotationChange *= -1;
+        }
+
+        float currentAngle = rotateOnX ? _cannon.rotation.eulerAngles.x : (_cannon.rotation.eulerAngles.y - 180);
+
+        if (currentAngle > 180)
+        {
+            currentAngle -= 360;
+        }
+
+        if (Mathf.Abs(currentAngle + rotationChange) > MaxCannonAngle)
+        {
+            rotationChange = currentAngle > 0 ? MaxCannonAngle - currentAngle : -(MaxCannonAngle + currentAngle);
+        }
+
+        Vector3 rotation = new Vector3(rotateOnX ? rotationChange : 0, rotateOnX ? 0 : rotationChange);
+        _cannon.Rotate(rotation);
+    }
+
+    float CapSpeed(float rawInput, float currentVelocity)
+    {
+        if (rawInput == 0)
+        {
+            return 0;
+        }
+
+        float absoluteSpeed = Mathf.Max(Mathf.Abs((rawInput * MaximumSpeed)), MaximumSpeed) - Mathf.Abs(currentVelocity);
+
+        return rawInput > 0 ? absoluteSpeed : -absoluteSpeed;
+    }
+
+    float CapRotation(float rawInput, float currentTorque)
+    {
+        if (rawInput == 0)
+        {
+            return 0;
+        }
+
+        float absoluteRotation = Mathf.Max(Mathf.Abs(rawInput * BodyTurnRate), BodyTurnRate);
+
+        return rawInput > 0 ? absoluteRotation : -absoluteRotation;
+    }
+
     void FixedUpdate()
     {
-        /*DesiredMovement = new Vector3
+        /*
+        //Temporarily get movement from user input
+        DesiredMovement = new Vector3
         (
             Input.GetAxis("Horizontal"),
             Input.GetAxis("Vertical"),
-            0
+            -Input.GetAxis("Pitch")
         );*/
 
-        float? newAngle = null;
-        if (Input.GetAxis("Vertical") > 0)
+        if (DesiredMovement.magnitude != 0)
         {
-            newAngle = CannonAimRate;
+            Vector3 localVelocity = _rigidBody.transform.InverseTransformDirection(_rigidBody.velocity);
+            Vector3 actualMovement = new Vector3
+            (
+                CapSpeed(DesiredMovement.x, localVelocity.x),
+                CapSpeed(DesiredMovement.y, localVelocity.y),
+                CapSpeed(DesiredMovement.z, localVelocity.z)
+            );
+
+            _rigidBody.AddRelativeForce(actualMovement);
         }
 
-        if (Input.GetAxis("Vertical") < 0)
+        /*
+        //Temporarily get rotation from user input
+        DesiredBodyRotation = new Vector3
+        (
+            Input.GetAxis("RotateVertical"),
+            Input.GetAxis("RotateHorizontal")
+        );*/
+
+        if (DesiredBodyRotation.magnitude != 0)
         {
-            newAngle = -CannonAimRate;
+            Vector3 localAngularVelocity = _rigidBody.transform.InverseTransformDirection(_rigidBody.angularVelocity);
+            Vector3 actualRotation = new Vector3
+            (
+                CapRotation(DesiredBodyRotation.x, localAngularVelocity.x), 
+                CapRotation(DesiredBodyRotation.y, localAngularVelocity.y), 
+                CapRotation(DesiredBodyRotation.z, localAngularVelocity.z)
+            );
+
+            _rigidBody.AddRelativeTorque(actualRotation);
         }
 
-        if (newAngle.HasValue)
+        /*
+        //Temporarily get cannon direction from user input
+        DesiredCannonRotation = new Vector2
+        (
+            Input.GetAxis("RotateVertical"),
+            Input.GetAxis("RotateHorizontal")
+        );*/
+
+        if (DesiredCannonRotation.x != 0)
         {
-            float currentAngle = _cannon.rotation.eulerAngles.x;
-            if (currentAngle > 180)
-            {
-                currentAngle = currentAngle - 360;
-            }
-            if (Mathf.Abs(currentAngle + newAngle.Value) <= MaxCannonAngle)
-            {
-                _cannon.Rotate(new Vector3(newAngle.Value, 0));
-            }
+            RotateCannon(DesiredCannonRotation.x, true);            
         }
 
-        //_rigidBody.AddForce(DesiredMovement * 10);
+        if (DesiredCannonRotation.y != 0)
+        {
+            RotateCannon(DesiredCannonRotation.y, false);
+        }
     }
 }
