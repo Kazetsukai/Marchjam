828f15757e3e3386f3b595f70c30426f36cdc7c2
joelectronics <righteouslyalgebraic@gmail.com>
2016-03-25 20:26:41 +1300

    Joe - Basic car physics. Ground checking

--- Assets/Resources/Scripts/Vehicle.cs
+++ Assets/Resources/Scripts/Vehicle.cs
@@ -4,19 +4,16 @@ using System.Collections;
 public class Vehicle : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] float ForwardForce = 100f;
-    [SerializeField] float TurnForce = 1000f;                //MUKLTIPLT BNY SPLIT LATER
+    [SerializeField] float DriveForce = 100f;
     [SerializeField] float MaxSteeringAngle = 35f;
-    [SerializeField] AnimationCurve CarSpeedVsCheapSteeringForce;
 
     [Header("Wheels")]
-    [SerializeField] GameObject Wheel_FR;
-    [SerializeField] GameObject Wheel_FL;
-    [SerializeField] GameObject Wheel_BR;
-    [SerializeField] GameObject Wheel_BL;
-    [SerializeField] AnimationCurve SpeedVsWheelLatForce;
+    [SerializeField] VehicleWheel Wheel_FR;
+    [SerializeField] VehicleWheel Wheel_FL;
+    [SerializeField] VehicleWheel Wheel_BR;
+    [SerializeField] VehicleWheel Wheel_BL;
 
-    Rigidbody rb;
+    public Rigidbody rb;
 
 	void Start ()
     {
@@ -29,29 +26,13 @@ public class Vehicle : MonoBehaviour
         float throttle = Input.GetAxis("Vertical");
         float steering = Input.GetAxis("Horizontal");
 
-        //Add rear drive force
-        rb.AddForceAtPosition(Wheel_BL.transform.forward * (throttle * ForwardForce * Time.deltaTime) / 2f, Wheel_BL.transform.position);
-        rb.AddForceAtPosition(Wheel_BR.transform.forward * (throttle * ForwardForce * Time.deltaTime) / 2f, Wheel_BR.transform.position);
+        //Add rear drive force   
+        Wheel_BL.DriveWheel((throttle * DriveForce * Time.deltaTime) / 2f);
+        Wheel_BR.DriveWheel((throttle * DriveForce * Time.deltaTime) / 2f);
 
         //Rotate wheels
         Wheel_FR.transform.localEulerAngles = new Vector3(Wheel_FR.transform.localEulerAngles.x, steering * MaxSteeringAngle, Wheel_FR.transform.localEulerAngles.z);
-        Wheel_FL.transform.localEulerAngles = new Vector3(Wheel_FL.transform.localEulerAngles.x, steering * MaxSteeringAngle, Wheel_FL.transform.localEulerAngles.z);
-        
-        // Lateral friction
-        var FRfarce = Vector3.Dot(rb.GetPointVelocity(Wheel_FR.transform.position), Wheel_FR.transform.up) * Wheel_FR.transform.up;
-        var FLfarce = Vector3.Dot(rb.GetPointVelocity(Wheel_FL.transform.position), Wheel_FL.transform.up) * Wheel_FL.transform.up;
-        var BRfarce = Vector3.Dot(rb.GetPointVelocity(Wheel_BR.transform.position), Wheel_BR.transform.up) * Wheel_BR.transform.up;
-        var BLfarce = Vector3.Dot(rb.GetPointVelocity(Wheel_BL.transform.position), Wheel_BL.transform.up) * Wheel_BL.transform.up;
-        
-        rb.AddForceAtPosition(-FRfarce * 0.25f, Wheel_FR.transform.position, ForceMode.VelocityChange);
-        rb.AddForceAtPosition(-FLfarce * 0.25f, Wheel_FL.transform.position, ForceMode.VelocityChange);
-        rb.AddForceAtPosition(-BRfarce * 0.25f, Wheel_BR.transform.position, ForceMode.VelocityChange);
-        rb.AddForceAtPosition(-BLfarce * 0.25f, Wheel_BL.transform.position, ForceMode.VelocityChange);                
-
-        Debug.DrawLine(Wheel_FR.transform.position, Wheel_FR.transform.position - FRfarce * 20);
-        Debug.DrawLine(Wheel_FL.transform.position, Wheel_FL.transform.position - FLfarce * 20);
-        Debug.DrawLine(Wheel_BR.transform.position, Wheel_BR.transform.position - BRfarce * 20);
-        Debug.DrawLine(Wheel_BL.transform.position, Wheel_BL.transform.position - BLfarce * 20);
+        Wheel_FL.transform.localEulerAngles = new Vector3(Wheel_FL.transform.localEulerAngles.x, steering * MaxSteeringAngle, Wheel_FL.transform.localEulerAngles.z);     
     }
     
     void Update ()
---
+++ Assets/Resources/Scripts/VehicleWheel.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleWheel : MonoBehaviour
+{
+    [SerializeField] Vehicle ParentVehicle;
+    [SerializeField] float Radius= 1f;
+    [SerializeField] float GroundedThreshold = 0.1f;
+    public bool Grounded = false;
+    [SerializeField] bool DebugRay = false;
+
+    [SerializeField] float FrictionLateral = 0.8f;              // Max value = 1f;
+    [SerializeField] float FrictionLateral_Drift = 0.1f;
+
+    RaycastHit hit;
+        
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        //Raycast down to see if wheel is touching something
+        if (Physics.Raycast(transform.position, transform.right, out hit))
+        {
+            Grounded = (hit.distance <= (Radius / 2f) + GroundedThreshold) ? true : false;
+
+            //Do lateral friction
+            if (Grounded)
+            {                
+                var force_lateral = Vector3.Dot(ParentVehicle.rb.GetPointVelocity(transform.position), transform.up) * transform.up;
+
+                float latFrictionFactor = FrictionLateral;
+
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    latFrictionFactor = FrictionLateral_Drift;
+                }
+                ParentVehicle.rb.AddForceAtPosition(-force_lateral * latFrictionFactor * 0.25f, transform.position, ForceMode.VelocityChange);          //Change 0.25f when changing weight transfer
+
+                if (DebugRay)
+                {
+                    Debug.DrawLine(transform.position, transform.position - force_lateral, Color.blue);
+                }
+            }
+
+            if (DebugRay)
+            {              
+                if (Grounded)
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.green);
+                }  
+                else
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.red);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Used to drive wheel. Positive force is forward rotation, negative is reverse.
+    /// </summary>
+    /// <param name="force"></param>
+    public void DriveWheel(float force)
+    {
+        if (Grounded)
+        {
+            ParentVehicle.rb.AddForceAtPosition(transform.forward * force, transform.position, ForceMode.Force);
+        }
+    }
+}
