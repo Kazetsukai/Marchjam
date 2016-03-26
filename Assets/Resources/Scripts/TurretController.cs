749e363938d5e613028257e12285f458c98faf10
Declan <declantvb@gmail.com>
2016-03-26 14:14:27 +1300

    Firing solutions now work when upside-down

--- Assets/Resources/Scripts/TurretController.cs
+++ Assets/Resources/Scripts/TurretController.cs
@@ -6,7 +6,7 @@ public class TurretController : MonoBehaviour
 	public GameObject projectilePrefab;
 
 	private const float maxTurretRotation = 180f;
-	private const float maxBarrelLift = 30f;
+	private const float maxBarrelLift = 45f;
 	private const float maxBarrelDrop = 15f;
 	private Transform _transform;
 	private Transform _car;
@@ -34,30 +34,33 @@ public class TurretController : MonoBehaviour
 		var mousePos = Input.mousePosition;
 		var worldMousePos = Camera.main.ScreenPointToRay(mousePos);
 		RaycastHit hitInfo;
-		Physics.Raycast(worldMousePos, out hitInfo);
+		var hit = Physics.Raycast(worldMousePos, out hitInfo);
 
-		var lookAt = hitInfo.point;
+		var targetPos = hit ? hitInfo.point : worldMousePos.GetPoint(100000);
 
-		var lookAtLocalCar = _car.InverseTransformPoint(lookAt);
+		var lookDir = targetPos - _barrel.position;
 
-		var lookDir = lookAtLocalCar - transform.localPosition;
+		Debug.DrawLine(_barrel.position, _barrel.position + lookDir, Color.red);
+
+		var lookY = lookDir.y;
 		lookDir.y = 0;
-		var rotation = Quaternion.LookRotation(lookDir, Vector3.up);
-		if (Quaternion.Angle(rotation, Quaternion.identity) >= maxTurretRotation)
-			rotation = Quaternion.RotateTowards(Quaternion.identity, rotation, maxTurretRotation);
-		transform.localRotation = Quaternion.RotateTowards(transform.localRotation, rotation, Time.deltaTime * damping);
-
-		var lookAtLocalTurret = _transform.InverseTransformPoint(lookAt);
-
-		var rawr = lookAtLocalTurret - _barrel.localPosition;
+		var lookNorm = lookDir.normalized;
 
 		var g = 9.81f; // gravity
 		var v = 40; // velocity
 		var x = lookDir.magnitude; // target x
-		var y = rawr.y; // target y
+		var y = lookY; // target y
 		var s = (v * v * v * v) - g * (g * (x * x) + 2 * y * (v * v)); //substitution
-		//var o1 = Mathf.Atan(((v * v) + Mathf.Sqrt(s)) / (g * x)) * Mathf.Rad2Deg; // launch angle
+		var o1 = Mathf.Atan(((v * v) + Mathf.Sqrt(s)) / (g * x)) * Mathf.Rad2Deg; // launch angle
 		var o2 = Mathf.Atan(((v * v) - Mathf.Sqrt(s)) / (g * x)) * Mathf.Rad2Deg; // launch angle
+
+		Debug.DrawRay(_barrel.position, Quaternion.AngleAxis(o2, Vector3.Cross(lookNorm, Vector3.up).normalized) * lookNorm * 5, Color.green);
+		Debug.DrawRay(_barrel.position, Quaternion.AngleAxis(o1, Vector3.Cross(lookNorm, Vector3.up).normalized) * lookNorm * 5, Color.cyan);
+		Debug.DrawLine(_barrel.position, _barrel.position + lookDir, Color.yellow);
+		Debug.DrawLine(_barrel.position + lookDir, _barrel.position + lookDir + new Vector3(0, lookY, 0), Color.magenta);
+
+
+		Vector3 firingDir = targetPos;
 
 		if (float.IsNaN(o2))
 		{
@@ -65,10 +68,37 @@ public class TurretController : MonoBehaviour
 		}
 		else
 		{
-			var lift = Quaternion.Euler(-o2, 0, 0);
-
-			_barrel.localRotation = Quaternion.RotateTowards(_barrel.localRotation, lift, Time.deltaTime * damping);
+			var firingAngle = Quaternion.AngleAxis(o2, Vector3.Cross(lookNorm, Vector3.up).normalized);
+			firingDir = firingAngle * lookNorm;
 		}
+
+		var localFiringDir = _car.InverseTransformVector(firingDir);
+
+		var yawDir = localFiringDir;
+		yawDir.y = 0;
+
+		var rotation = Quaternion.LookRotation(yawDir.normalized, Vector3.up);
+
+		//if (Quaternion.Angle(rotation, Quaternion.identity) >= maxTurretRotation)
+		//	rotation = Quaternion.RotateTowards(Quaternion.identity, rotation, maxTurretRotation);
+
+		transform.localRotation = Quaternion.RotateTowards(transform.localRotation, rotation, Time.deltaTime * damping);
+
+		var lookAtLocalTurret = _transform.InverseTransformVector(firingDir);
+
+		var liftDir = lookAtLocalTurret;
+		liftDir.x = 0;
+		var lift = Quaternion.LookRotation(liftDir.normalized, Vector3.up);
+
+		var clampAngle = liftDir.y > 0 ? maxBarrelLift : maxBarrelDrop;
+		if (Quaternion.Angle(lift, Quaternion.identity) >= clampAngle)
+			lift = Quaternion.RotateTowards(Quaternion.identity, lift, clampAngle);
+
+		print(Quaternion.Angle(lift, Quaternion.identity));
+		_barrel.localRotation = Quaternion.RotateTowards(_barrel.localRotation, lift, Time.deltaTime * damping);
+
+
+
 
 		var mousedown = Input.GetAxis("Fire1");
 
