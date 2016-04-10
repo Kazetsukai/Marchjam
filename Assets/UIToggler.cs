f44da4c2ee445f1a52a035ec16021c8d3dc8a8b4
Daniel Tebbutt <moeruze.burning@gmail.com>
2016-04-10 12:45:12 +1200

    Added F2 to toggle network HUD

---
+++ Assets/UIToggler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class UIToggler : MonoBehaviour {
+
+    NetworkManagerHUD _hud;
+
+	// Use this for initialization
+	void Start () {
+        _hud = GetComponent<NetworkManagerHUD>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.F2))
+        {
+
+            _hud.showGUI = !_hud.showGUI;
+        }
+	}
+}
