b892cb85c0780d61b93203e8cb17b8c552052e83
Daniel Tebbutt <moeruze.burning@gmail.com>
2016-04-10 17:30:08 +1200

    More little tweaks

---
+++ Assets/PlayerNameDisplay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class PlayerNameDisplay : MonoBehaviour {
+    private RectTransform _textRect;
+
+    public Transform Target;
+    private Text _text;
+
+    // Use this for initialization
+    void Start () {
+        _textRect = GetComponentInChildren<RectTransform>();
+        _text = GetComponent<Text>();
+
+        _text.text = Environment.MachineName;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var screenPoint = Camera.main.WorldToScreenPoint(Target.position + Vector3.up * 1.5f);
+        _textRect.anchoredPosition = screenPoint;
+        Debug.Log(screenPoint);
+	}
+}
